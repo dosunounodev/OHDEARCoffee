e0fbdd6e91454bf9ee3a7c285b05041b2c3c25d7
Martin Fernandez <dosunounodev@gmail.com>
2021-03-09 19:30:17 -0300

    ids fix en menu js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,7 +3,24 @@ console.log('hola')
 const navMenuBtn = document.getElementById('nav__menu-btn')
 const navMenuList = document.getElementById('nav__menu-lists')
 
+const inicioBtn = document.getElementById('btnheader')
+const bioBtn = document.getElementById('btnbio')
+const blogBtn = document.getElementById('btnblog')
+const recetasBtn = document.getElementById('btnrecetas')
+const cursosBtn = document.getElementById('btncursos')
+const guiasBtn = document.getElementById('btnguias')
+const contactoBtn = document.getElementById('btncontacto')
+const newslatterBtn = document.getElementById('btnnewslatter')
+
 navMenuBtn.addEventListener('click', hideShow)
+inicioBtn.addEventListener('click', hideShow)
+bioBtn.addEventListener('click', hideShow)
+blogBtn.addEventListener('click', hideShow)
+recetasBtn.addEventListener('click', hideShow)
+cursosBtn.addEventListener('click', hideShow)
+guiasBtn.addEventListener('click', hideShow)
+contactoBtn.addEventListener('click', hideShow)
+newslatterBtn.addEventListener('click', hideShow)
 
 function hideShow() {
   if(navMenuBtn.classList.contains('menu-active')) {
